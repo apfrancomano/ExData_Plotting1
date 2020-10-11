5c77ef873914f5b566c60571a59747934490c602
apfrancomano <agus_francomano@hotmail.com>
2020-10-10 21:32:44 -0300

    Subo último cambio para que se entienda

--- Plot3.R
+++ Plot3.R
@@ -1,3 +1,21 @@
+file <- read.table("household_power_consumption.txt", header=TRUE, sep=";",
+                   na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+file$Date <- as.Date(file$Date, "%d/%m/%Y")
+
+file <- subset(file,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+file <- file[complete.cases(file),]
+
+dateTime <- paste(file$Date, file$Time)
+
+dateTime <- setNames(dateTime, "DateTime")
+file <- file[ ,!(names(file) %in% c("Date","Time"))]
+
+file <- cbind(dateTime, file)
+
+file$dateTime <- as.POSIXct(dateTime)
+
 with(file, {
   plot(Sub_metering_1~dateTime, type="l",
        ylab="Energy sub metering", xlab="")
--- Plot4.R
+++ Plot4.R
@@ -1,3 +1,21 @@
+file <- read.table("household_power_consumption.txt", header=TRUE, sep=";",
+                   na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+file$Date <- as.Date(file$Date, "%d/%m/%Y")
+
+file <- subset(file,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+file <- file[complete.cases(file),]
+
+dateTime <- paste(file$Date, file$Time)
+
+dateTime <- setNames(dateTime, "DateTime")
+file <- file[ ,!(names(file) %in% c("Date","Time"))]
+
+file <- cbind(dateTime, file)
+
+file$dateTime <- as.POSIXct(dateTime)
+
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(file, {
   plot(Global_active_power~dateTime, type="l", 
